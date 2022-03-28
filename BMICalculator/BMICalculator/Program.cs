402bb2856925dadb1ba9c011152e32082a3c093e
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-03-28 16:26:09 +0300

    BMI Last

--- BMICalculator/BMICalculator/Program.cs
+++ BMICalculator/BMICalculator/Program.cs
@@ -17,10 +17,31 @@ namespace BMICalculator
                 double height = Convert.ToDouble(inputHeight);
 
                 double bmi = weight / (height * height);
-                if (double.IsFinite(bmi))
+                //bmi değeri sonlu bir değerse
+                if (double.IsFinite(bmi) && bmi > 0)
                 {
-                    Console.WriteLine("BMI değeriniz: " + bmi + " bilginize");
-                    Console.WriteLine($"BMI değeriniz: {bmi}\nbilginize!");
+                    //Console.WriteLine("BMI değeriniz: " + bmi + " bilginize");
+                    Console.WriteLine($"BMI değeriniz: {bmi} bilginize!");
+
+                    if (bmi < 18.5)
+                    {
+                        Console.WriteLine("Çok zayıfsınız!");
+                    }
+                    else if (bmi <=24.9)
+                    {
+                        Console.WriteLine("Normalsiniz");
+                    }
+                    else if (bmi <=29.9 )
+                    {
+                        Console.WriteLine("Biraz kilolusunuz");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Obez");
+                    }
+
+
+
 
                 }
                 else
@@ -38,7 +59,13 @@ namespace BMICalculator
             {
                 Console.WriteLine();
             }
+            catch(Exception ex)
+            {
+                if (ex is DivideByZeroException)
+                {
 
+                }
+            }
 
         }
     }
