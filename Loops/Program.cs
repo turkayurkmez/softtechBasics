b7a1f34caeec11b4a9dcf10ad36fbce0d4d7189a
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-03-29 13:46:03 +0300

    doWhile loop explained

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -6,9 +6,24 @@ namespace loops
     {
         static void Main(string[] args)
         {
+            //Ctrl + K  + C 
 
-            bool uygulamaDevamEtsinMi = true ;
-            while (uygulamaDevamEtsinMi)
+            // bool uygulamaDevamEtsinMi = true ;
+            // while (uygulamaDevamEtsinMi)
+            // {
+            //     Console.WriteLine("Bir sayi giriniz:");
+            //     int num1 = Convert.ToInt32(Console.ReadLine());
+            //     Console.WriteLine("Bir sayi daha giriniz:");
+            //     int num2 = Convert.ToInt32(Console.ReadLine());
+
+            //     int carpim = num1 * num2;
+            //     Console.WriteLine($"Çarpım sonucu: {carpim}");
+            //     System.Console.WriteLine("Bir işlem daha yapmak ister misiniz (E/H) ?");
+            //     uygulamaDevamEtsinMi = Console.ReadLine() == "E";
+            // }
+
+            //aynı işin alternatifi:
+            do
             {
                 Console.WriteLine("Bir sayi giriniz:");
                 int num1 = Convert.ToInt32(Console.ReadLine());
@@ -18,8 +33,7 @@ namespace loops
                 int carpim = num1 * num2;
                 Console.WriteLine($"Çarpım sonucu: {carpim}");
                 System.Console.WriteLine("Bir işlem daha yapmak ister misiniz (E/H) ?");
-                uygulamaDevamEtsinMi = Console.ReadLine() == "E";
-            }
+            } while (Console.ReadLine()=="E");
 
 
 
