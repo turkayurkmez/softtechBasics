71bfe58e15efe1157524b4e3949f5202de9ae322
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-03-29 12:31:36 +0300

    loops initialized

---
+++ Loops/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool uygulamaDevamEtsinMi = true ;
+            while (uygulamaDevamEtsinMi)
+            {
+                Console.WriteLine("Bir sayi giriniz:");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Bir sayi daha giriniz:");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                int carpim = num1 * num2;
+                Console.WriteLine($"Çarpım sonucu: {carpim}");
+                System.Console.WriteLine("Bir işlem daha yapmak ister misiniz (E/H) ?");
+                uygulamaDevamEtsinMi = Console.ReadLine() == "E";
+            }
+
+
+
+        }
+    }
+}
