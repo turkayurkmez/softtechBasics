f7023e2e5c46712fe0f6b8108b38475939c4dce6
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-03-30 13:55:46 +0300

    adding params keyword for array argument

--- MethodDetails/MethodDetails/Program.cs
+++ MethodDetails/MethodDetails/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace MethodDetails
 {
@@ -16,6 +17,41 @@ namespace MethodDetails
             var squareAlternative = getAreaAlt(5);
             var circleAreae = getAreaAlt(8, sekil: "daire");
             var triangleArea = getAreaAlt(unitLength1: 10, unitLength2: 4, sekil: "üçgen");
+
+            int modResult = 0;
+            var divideResult = divide(19, 4, out modResult);
+            Console.WriteLine($"Bölüm sonucu:{divideResult}, kalan: {modResult}");
+            Console.ReadLine();
+
+            Console.WriteLine("Bir sayı giriniz");
+            string inputValue = Console.ReadLine();
+            double aNumber = 0;
+            if (double.TryParse(inputValue,out aNumber))
+            {
+                sample(ref aNumber);
+
+                Console.WriteLine($"main metodundaki aNumber değeri : {aNumber}");
+            }
+
+            //A000BBB111
+            string[] names = { "Feyza", "Hasan", "İnci" };
+            Array.Resize(ref names, 4);
+            /*
+                string[] names = { "Feyza", "Hasan", "İnci","" };
+             * 
+             */
+            names[3] = "Türkay";
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            int total = sum(13, 9, 122, 46, -3,45);
+            Console.WriteLine($"toplam: {total}");
+           
+
+
         }
 
         static double getArea(double unitLength1, string sekil)
@@ -54,7 +90,6 @@ namespace MethodDetails
             }
             return result;
         }
-
         static double getAreaAlt(double unitLength1, double unitLength2 = 1, string sekil = "kare")
         {
             double result = 0.0;
@@ -80,6 +115,23 @@ namespace MethodDetails
             return result;
         }
 
+        static int divide(int x, int y, out int modulo)
+        {
+            modulo = x % y;
+            return x / y;
+        }
+
+        static void sample(ref double number)
+        {
+            number = Math.Pow(number, 2);
+            Console.WriteLine($"sample metodundaki değer:{ number}");
+
+        }
+
+        static int sum(params int[] numbers)
+        {
+            return numbers.ToList().Sum();
+        }
 
     }
 }
